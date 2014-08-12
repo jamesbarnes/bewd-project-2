97e330128a524249cc772f65a7520ed80184e067
jamesbarnes <james.merritt.barnes@gmail.com>
2014-08-12 10:28:42 -0400

    added migration and modified schema

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140720145130) do
+ActiveRecord::Schema.define(version: 20140811163940) do
 
   create_table "nodes", force: true do |t|
     t.string   "name"
@@ -19,6 +19,7 @@ ActiveRecord::Schema.define(version: 20140720145130) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.integer  "project_id"
+    t.integer  "parent_id"
   end
 
   add_index "nodes", ["project_id"], name: "index_nodes_on_project_id"
